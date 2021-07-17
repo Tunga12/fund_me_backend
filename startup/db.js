e51d2ce28101fc255b1fe7637f6b54d5efe571d7
Hanan Mohsin <hananmohsin967088@gmail.com>
2021-07-17 16:08:04 +0300

    added connection string 2

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Fawn = require('fawn');
 
 module.exports = function(){
-	mongoose.connect('mongodb+srv://crowd:BavnzHY02VgNLApN@crowdcluster.yw6fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+	mongoose.connect('mongodb+srv://crowd:BavnzHY02VgNLApN@crowdcluster.yw6fz.mongodb.net/crowd?retryWrites=true&w=majority',{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
@@ -12,6 +12,6 @@ module.exports = function(){
     .then(() => winston.info('Connected to the database...'))
     .catch((err) => console.error('Error',err));
 
-    Fawn.init('mongodb+srv://crowd:BavnzHY02VgNLApN@crowdcluster.yw6fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    Fawn.init('mongodb+srv://crowd:BavnzHY02VgNLApN@crowdcluster.yw6fz.mongodb.net/crowd?retryWrites=true&w=majority');
     
 }
