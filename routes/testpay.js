8946c6f53c4c138f7cf71bb011329e7eab2daf0f
Hanan Mohsin <hananmohsin967088@gmail.com>
2021-09-07 19:44:18 +0300

    telebirr trial 2

--- routes/testpay.js
+++ routes/testpay.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const axios = require('axios');
+const http = require('http');
 const qs = require('querystring');
 const crypto = require('crypto');
 const NodeRSA = require('node-rsa')
 const winston = require('winston');
 const router = express();
 
-const axios = require('axios');
 
 const stringA = 'appId=4347b88db6e64e0baa9e588acd42d50c&appKey=ffbf324b21974d778cec063f17aa1367&nonce=8a743de3ae3346e9920ecc46200226dd&notifyUrl=http://www.google.com&outTradeNo=426af391569b4e39b449d24b30bef93a&receiveName=OrgName&returnUrl=http://www.google.com&shortCode=410028&subject=Goods Name&timeoutExpress=30&timestamp=1624546517701&totalAmount=10';
 
 const sign = crypto.createHash('sha256')
-			.update(stringA.toUpperCase())
+			.update(stringA)
 			.digest('base64');
 			
 const key= new NodeRSA(
@@ -30,28 +29,38 @@ const key= new NodeRSA(
 						
 const ussd = key.encrypt('{"appId":"4347b88db6e64e0baa9e588acd42d50c","nonce":"8a743de3ae3346e9920ecc46200226dd","notifyUrl":"http://www.google.com","outTradeNo":"426af391569b4e39b449d24b30bef93b","receiveName":"Org Name","returnUrl":"http://www.google.com","shortCode":"410028","subject":"Goods Name","timeoutExpress":"30","timestamp":"1624546517701","totalAmount":"10"}','base64');
 
-const data = {
+const data = qs.stringify({
     appid: '4347b88db6e64e0baa9e588acd42d50c',
-    sign: sign,
+    sign: sign.toUpperCase(),
 	ussd:ussd
-};
+});
 
+var options = {
+      host: 'closure-compiler.appspot.com',
+      port: '80',
+      path: '/compile',
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json;charset=utf-8',
+          'Content-Length': Buffer.byteLength(data)
+      }
+  };
+  
 
-const config = {
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    }
-};
 
 // Post a help
 router.post('/',async(req,res) => {
     
-    axios.post('http://196.188.120.3:11443/ammapi/service-openup/toTradeWebPay', qs.stringify(data), config)
-    .then((res) => {
-		winston.info(`Status: ${res.status}`);
-        winston.info('Body: ', res.data);
- 
-    }).catch((err) => {
-        winston.info('Telebirr error: ', err);
-    });
+     var post_req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+  post_req.write(data);
+ post_req.end();
 });
+
+
+  
+module.exports = router;
