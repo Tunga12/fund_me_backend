f629c1f25dc62a85d417b01f271056af5e23fd74
Tunga Tessema <tungat72@gmail.com>
2021-10-15 17:27:36 +0300

    trying  telebirr

--- routes/telebirr.js
+++ routes/telebirr.js
@@ -4,38 +4,66 @@ const axios = require('axios').default;
 const timestamp = require('unix-timestamp');
 const { v4: uuidv4 } = require('uuid');
 
+const http = require('http')
+
 // sent form web app
 router.post('/pay', (req, res) => {
 
     // validate request
 
     // send request to telebirr server
-    axios.post("http://196.188.120.3:11443/ammapi/service-openup/toTradeWebPay",
-        {
-            "appId": "4347b88db6e64e0baa9e588acd42d50c",
-            "timestamp": timestamp.now(),
-            "nonce": uuidv4(),
-            "returnUrl": req.body.returnUrl,
-            "notifyUrl": "legas.highlight-group.com/api/telebirr/result",
-            "subject": req.body.subject,
-            "outTradeNo": uuidv4(),
-            "timeoutExpress": "5",
-            "totalAmount": req.body.totalAmount,
-            "shortCode": "410028",
-            "receiveName": "Highlight Software Design",
-        },
-    ).then((response) => {
-        res.send(response.data)
-    }).catch((error) => {
-        res.send(error)
-    });
+    // http.post("http://196.188.120.3:11443/ammapi/service-openup/toTradeWebPay",
+    //     {
+    //         "appId": "4347b88db6e64e0baa9e588acd42d50c",
+    //         "timestamp": timestamp.now(),
+    //         "nonce": uuidv4(),
+    //         "returnUrl": req.body.returnUrl,
+    //         "notifyUrl": "legas.highlight-group.com/api/telebirr/result",
+    //         "subject": req.body.subject,
+    //         "outTradeNo": uuidv4(),
+    //         "timeoutExpress": "5",
+    //         "totalAmount": req.body.totalAmount,
+    //         "shortCode": "410028",
+    //         "receiveName": "Highlight Software Design",
+    //     }
+    // ).then((response) => {
+    //     res.send(response.data)
+    // }).catch((error) => {
+    //         res.send(error)
+    //     });
 
+    const teleReq = http.request("http://196.188.120.3:11443/ammapi/service-openup/toTradeWebPay",
+        {
+            method: "POST",
+            data: {
+                "appId": "4347b88db6e64e0baa9e588acd42d50c",
+                "timestamp": timestamp.now(),
+                "nonce": uuidv4(),
+                "returnUrl": req.body.returnUrl,
+                "notifyUrl": "legas.highlight-group.com/api/telebirr/result",
+                "subject": req.body.subject,
+                "outTradeNo": uuidv4(),
+                "timeoutExpress": "5",
+                "totalAmount": req.body.totalAmount,
+                "shortCode": "410028",
+                "receiveName": "Highlight Software Design",
+            }
+        },
+        (response) => {
+            res.send(response.data)
+        },
+    )
+
+    teleReq.on('error', error => {
+        res.send(error)
+    })
 
 })
 
 // sent from telebirr server
 router.post('/result', (req, res) => {
 
+    res.send(req.body)
 })
 
 
