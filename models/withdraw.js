7d25bd4e971215ae87cc32a97899f4e1e8143466
Tunga Tessema <tungat72@gmail.com>
2022-03-05 16:19:54 +0300

    withdraw

--- models/withdraw.js
+++ models/withdraw.js
@@ -1,61 +1,58 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const withdrawSchema = new mongoose.Schema({
-	bankName: {
-		type: String,
-		required: true
-	},
-	bankAccountNo: {
-		type: String,
-		unique:true,
-		required: true
-	},
-	isOrganizer: {
-		type: Boolean,
-		required: true
-	},
-	status: {
-		type: String,
-		default: 'pending'
-	},
-	reason: {
-		type: String
-	},
-	date: {
-		type: Date,
-        default: Date.now,
-	},
-	isDeleted: {
-        type: Boolean,
-        default: false
-    }
-	
+  bankName: {
+    type: String,
+    required: true,
+  },
+  bankAccountNo: {
+    type: String,
+    required: true,
+  },
+  isOrganizer: {
+    type: Boolean,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  reason: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
 });
-withdrawSchema.plugin(require('mongoose-paginate-v2'));
-const Withdraw = mongoose.model('Withdraw',withdrawSchema);
+withdrawSchema.plugin(require("mongoose-paginate-v2"));
+const Withdraw = mongoose.model("Withdraw", withdrawSchema);
 
-function validateWithdraw(withdraw){
-    const schema = Joi.object({
-        bankName: Joi.string().required(),
-        bankAccountNo: Joi.string().required(),
-		isOrganizer: Joi.boolean().required(),
-		status: Joi.string(),
-		reason: Joi.string(),
-		date: Joi.date(),
-        isDeleted: Joi.boolean()
+function validateWithdraw(withdraw) {
+  const schema = Joi.object({
+    bankName: Joi.string().required(),
+    bankAccountNo: Joi.string().required(),
+    isOrganizer: Joi.boolean().required(),
+    status: Joi.string(),
+    reason: Joi.string(),
+    date: Joi.date(),
+    isDeleted: Joi.boolean(),
+  });
 
-    });
-
-    return schema.validate(withdraw);
+  return schema.validate(withdraw);
 }
 
-function getPagination (page, size)  {
-    const limit = size ? +size : 10;
-    const offset = page ? page * limit : 0;
-  
-    return { limit, offset };
-};
+function getPagination(page, size) {
+  const limit = size ? +size : 10;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+}
 
 module.exports.Withdraw = Withdraw;
 module.exports.validate = validateWithdraw;
