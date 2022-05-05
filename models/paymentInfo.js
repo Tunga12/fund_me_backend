2824a858937b23fe9587a3f770c02b9891f50828
Tunga Tessema <tungat72@gmail.com>
2022-05-05 13:38:59 +0300

    short code routes

--- models/paymentInfo.js
+++ models/paymentInfo.js
@@ -2,6 +2,10 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 
 const paymentInfoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   shortcodeTelebirr: {
     type: Number,
     required: true,
@@ -24,6 +28,7 @@ const PaymentInfo = mongoose.model("PaymentInfo", paymentInfoSchema);
 
 function validatePaymentInfo(paymentInfo) {
   const schema = Joi.object({
+    name: Joi.string().required(),
     shortcodeTelebirr: Joi.number().required(),
     appIdTelebirr: Joi.string().required(),
     appKeyTelebirr: Joi.string().required(),
