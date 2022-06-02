b3339f3c535ce9bc15337c99edef58d0e10eb61f
Tunga Tessema <tungat72@gmail.com>
2022-06-02 12:17:21 +0300

    remove registration

--- models/donation.js
+++ models/donation.js
@@ -5,7 +5,7 @@ const donationSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    // required: true,
   },
   memberId: {
     type: mongoose.Schema.Types.ObjectId,
