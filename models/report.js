473260a7c0183234fd6d04e6c287183ee96c3474
Tunga Tessema <tungat72@gmail.com>
2022-06-03 11:13:41 +0300

    reports

--- models/report.js
+++ models/report.js
@@ -1,45 +1,108 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const reportSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-	fundraiserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Fundraiser',
-        required: true,
-    },
-	reason:{
-       type: mongoose.Schema.Types.ObjectId,
-        ref: 'ReportReason',
-        required: true,
-    },
-	date: {
-        type: Date,
-        default: Date.now,
-    },
-    isDeleted: {
-        type: Boolean,
-        default: false
-    }
+  //   userId: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User",
+  //     required: true,
+  //   },
+  //   fundraiserId: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Fundraiser",
+  //     required: true,
+  //   },
+  //   reason: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "ReportReason",
+  //     required: true,
+  //   },
+  //   date: {
+  //     type: Date,
+  //     default: Date.now,
+  //   },
+  //   isDeleted: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+
+  // new
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  knowsOrganizer: {
+    type: Boolean,
+    required: true,
+  },
+  knowsDescription: {
+    type: String,
+  },
+  reasonType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ReportReason",
+    required: true,
+  },
+  reasonDescription: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "resolved", "lacksinfo"],
+    lowercase: true,
+    default: "pending",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Report = mongoose.model('Report', reportSchema);
+const Report = mongoose.model("Report", reportSchema);
 
-function validateReport(report){
-    const schema = Joi.object({
-        userId: Joi.objectId().required(),
-		fundraiserId: Joi.objectId().required(),
-		reason: Joi.objectId().required(),
-		date: Joi.date(),
-		isDeleted: Joi.boolean()
-    });
+function validateReport(report) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    phone: Joi.string().min(10).required(),
+    email: Joi.string().email().required(),
+    url: Joi.string().required(),
+    knowsOrganizer: Joi.boolean().required(),
+    knowsDescription: Joi.string(),
+    reasonType: Joi.objectId().required(),
+    reasonDescription: Joi.string().required(),
+    userId: Joi.objectId().required(),
+    status: Joi.string().valid("pending", "resolved", "lacksinfo"),
+  });
 
-    return schema.validate(report);
+  return schema.validate(report);
+}
+
+function validateUpdate(report) {
+  const schema = Joi.object({
+    status: Joi.string().valid("pending", "resolved", "lacksinfo").required(),
+  });
+
+  return schema.validate(report);
 }
 
 module.exports.Report = Report;
 module.exports.validate = validateReport;
+module.exports.validateUpdate = validateUpdate;
--- routes/reports.js
+++ routes/reports.js
@@ -1,64 +1,89 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const {Report,validate} = require('../models/report');
-const {auth} = require('../middleware/auth');
-const admin = require('../middleware/admin');
+const mongoose = require("mongoose");
+const express = require("express");
+const { Report, validate, validateUpdate } = require("../models/report");
+const { auth } = require("../middleware/auth");
+const admin = require("../middleware/admin");
 
 const router = express();
 //helps
 //help
 // Get all reports
-router.get('/', [auth,admin],async(req,res) => {
-    const reports = await Report.find({isDeleted: false})
-	.sort('-date').select('-isDeleted')
-	.populate('reason','name');
-    res.send(reports);
+router.get("/", [auth, admin], async (req, res) => {
+  const PendingReports = await Report.find({ status: "pending" })
+    .sort("-date")
+    .populate("reasonType", "name");
+
+  const LacksInfoReports = await Report.find({ status: "lacksinfo" })
+    .sort("-date")
+    .populate("reasonType", "name");
+
+  const ResolvedReports = await Report.find({ status: "resolved" })
+    .sort("-date")
+    .populate("reasonType", "name");
+
+  res.send({
+    pending: PendingReports,
+    laksInfo: LacksInfoReports,
+    resolved: ResolvedReports,
+  });
 });
 
-
-router.get('/:id',[auth,admin],async(req,res) => {
-	 
-    const report = await Report.findOne({_id: req.params.id, isDeleted:false})
-	.select('-isDeleted')
-	.populate('reason','name');
-	if(!report) return res.status(404).send('Report with the given ID was not found.');
-    res.send(report);
+router.get("/:id", [auth, admin], async (req, res) => {
+  const report = await Report.findOne({ _id: req.params.id }).populate(
+    "reasonType",
+    "name"
+  );
+  if (!report)
+    return res.status(404).send("Report with the given ID was not found.");
+  res.send(report);
 });
 
 // Post a report
-router.post('/', auth,async(req,res) => {
-	req.body.userId = req.user._id;
-    const {error} = validate(req.body);
-	if(error) return res.status(400).send(error.details[0].message);
+router.post("/", auth, async (req, res) => {
+  req.body.userId = req.user._id;
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let report = new Report(req.body);
-    report = await report.save();
+  let report = new Report(req.body);
+  report = await report.save();
 
-    res.send(report);
+  res.send(report);
 });
 
-// Update a report
-router.put('/:id',auth,async(req,res) => {
-	
-    const {error} = validate(req.body);
-	if(error) return res.status(400).send(error.details[0].message);
+// Update the status of the report
+router.put("/:id", [auth, admin], async (req, res) => {
+  const { error } = validateUpdate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const report = await Report.findByIdAndUpdate(req.params.id,req.body,{new: true});
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid id");
+  }
 
-    if (!report) return res.status(404).send('Report with the given ID was not found.');
+  const report = await Report.findByIdAndUpdate(
+    req.params.id,
+    { status: req.body.status },
+    {
+      new: true,
+    }
+  );
 
-    res.send(report);
+  if (!report)
+    return res.status(404).send("Report with the given ID was not found.");
+
+  res.send(report);
 });
 
-router.delete('/:id',auth,async(req, res) => {
-	
-    const report = await Report.findByIdAndUpdate(req.params.id,{isDeleted: true},{new: true});
+router.delete("/:id", auth, async (req, res) => {
+  const report = await Report.findByIdAndUpdate(
+    req.params.id,
+    { isDeleted: true },
+    { new: true }
+  );
 
-    if (!report) return res.status(404).send('Report with the given ID was not found.');
+  if (!report)
+    return res.status(404).send("Report with the given ID was not found.");
 
-    res.send('Report is deleted') 
-
+  res.send("Report is deleted");
 });
-
 
 module.exports = router;
